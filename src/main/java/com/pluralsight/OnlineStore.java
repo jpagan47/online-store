d0e6b4e83a3c22c6aaa8e17b4928b528e76bdef0
josepagan <josepaganrnj@outlook.com>
2026-04-24 09:41:24 -0400

    Successfully split the lines along the "|" and have made a new product instance as well.

--- src/main/java/com/pluralsight/OnlineStore.java
+++ src/main/java/com/pluralsight/OnlineStore.java
@@ -3,6 +3,7 @@ package com.pluralsight;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class OnlineStore {
     public static void main(String[] args) {
@@ -10,14 +11,29 @@ public class OnlineStore {
 
         String filePath = "src/main/resources/products.csv";
         FileReader fileReader;
-            try {
-                fileReader = new FileReader(filePath);
-                BufferedReader bufferedReader = new BufferedReader(fileReader);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+        try {
+            fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
         BufferedReader bufferedReader = new BufferedReader(fileReader);
+        try {
+            String line = bufferedReader.readLine();
+            while (line != null){
+                String[] parts = line.split("\\|");
+                String sku = parts[0];
+                String productName = parts[1];
+                double price = Double.parseDouble(parts[2]);
+                String department = parts[3];
+
+                Product product = new Product(sku,productName,price,department);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
