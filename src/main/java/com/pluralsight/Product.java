6a77ac76b6d2a20af0ab7e0ccdc541e36187e6bd
josepagan <josepaganrnj@outlook.com>
2026-04-24 09:20:23 -0400

    Added "Product" class fields and constructed my getters and setter along with my constructor

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,52 @@
 package com.pluralsight;
 
 public class Product {
+    public String sku;
+
+    public Product(String sku, String productName, double price, String department) {
+        this.sku = sku;
+        this.productName = productName;
+        this.price = price;
+        this.department = department;
+    }
+
+    public String productName;
+    public double price;
+    public String department;
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+
+
+
 }
