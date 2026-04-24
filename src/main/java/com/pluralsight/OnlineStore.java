fe08d0fe783c4e9aaab402a0cf05a6b78c2739b9
josepagan <josepaganrnj@outlook.com>
2026-04-24 09:32:19 -0400

    Added Filereader and Bufferedreader to my "OnlineStoreApp"

--- src/main/java/com/pluralsight/OnlineStore.java
+++ src/main/java/com/pluralsight/OnlineStore.java
@@ -1,5 +1,24 @@
 package com.pluralsight;
 
-public class OnlineStore {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
+public class OnlineStore {
+    public static void main(String[] args) {
+
+
+        String filePath = "src/main/resources/products.csv";
+        FileReader fileReader;
+            try {
+                fileReader = new FileReader(filePath);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+
+    }
 }
